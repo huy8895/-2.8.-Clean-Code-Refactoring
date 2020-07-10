6bfd5ccd018329a6d09d97a0fd532202f314d090
huy8895 <huy8895@gmail.com>
2020-07-10 20:14:41 +0700

    [Thực hành] Refactoring - tách biến

---
+++ src/refatoringtachbien/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+package refatoringtachbien;
+
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        System.out.println("FizzBuzz.fizzBuzz(2) = " + FizzBuzz.fizzBuzz(2));
+        System.out.println("FizzBuzz.fizzBuzz(2) = " + FizzBuzz.fizzBuzz(6));
+    }
+
+}
