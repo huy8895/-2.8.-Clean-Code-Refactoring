51046a14323dc399fffa6f9a745993a49c260ebf
huy8895 <huy8895@gmail.com>
2020-07-10 19:53:24 +0700

    [Thực hành] Refactoring - đổi tên biến và tách hằng

---
+++ src/CalculatorTest.java
@@ -0,0 +1,7 @@
+public class CalculatorTest {
+    public static void main(String[] args) {
+        int result;
+        result = Calculator.calculate(1,2,'+');
+        System.out.println("result = " + result);
+    }
+}
