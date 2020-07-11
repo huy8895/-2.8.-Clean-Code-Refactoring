e89acf3a00779815e14fc0ed8c1307add5f5881e
huy8895 <huy8895@gmail.com>
2020-07-11 15:35:48 +0700

    tennis game refactoring

--- src/baitaprefactoring/TestTennis.java
+++ src/baitaprefactoring/TestTennis.java
@@ -2,7 +2,7 @@ package baitaprefactoring;
 
 public class TestTennis {
     public static void main(String[] args) {
-        String score = TennisGame.getScore(1, 1);
+        String score = TennisGame.getScore(4, 4);
         System.out.println("score = " + score);
     }
 }
